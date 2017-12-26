7a6757547bc817d6919d6e62ba3ece162a8b6865
wangchen <1634718783@qq.com>
2017-12-26 17:25:32 +0800

    主页面

---
+++ src/views/homepage/homepage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <hometop></hometop>
+  </div>
+</template>
+<script>
+import hometop from './top.vue'
+export default {
+  components: {
+    hometop
+  }
+}
+</script>
+<style scoped>
+
+</style>
