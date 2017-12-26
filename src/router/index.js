b473d25f591d63f3bbf812b35d440342d0562273
wangchen <1634718783@qq.com>
2017-12-26 17:26:28 +0800

    路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import homepage from '../views/homepage/homepage.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {path: '/', redirect: 'homepage'},
+    {path: '/homepage', component: homepage}
   ]
 })
