544878ecbb40a80d8f275fde208f44ea8cf5a31a
wangchen <1634718783@qq.com>
2017-12-27 17:17:44 +0800

    主页顶部

---
+++ src/views/homepage/topcontent/advert.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="advert">
+    <img v-for="(item,index) in arr" :key="index" :src="item.img">
+  </div>
+</template>
+<script>
+
+export default {
+  data () {
+    return {
+      arr: [
+        {img: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1644077758,3194768429&fm=200&gp=0.jpg'},
+        {img: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1644077758,3194768429&fm=200&gp=0.jpg'},
+        {img: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1644077758,3194768429&fm=200&gp=0.jpg'},
+        {img: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1644077758,3194768429&fm=200&gp=0.jpg'}
+      ]
+    }
+  }
+}
+</script>
+<style scoped>
+.advert{
+  word-wrap: break-word;
+  width: 296px;
+  height: 360px;
+}
+.advert img{
+  float: left;
+  width: 100%;
+  height: 90px;
+}
+</style>
---
+++ src/views/homepage/topcontent/menu.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="menu">
+    <div >
+      <ul>
+        <li>
+          <p class="are-con-left-p1"><a>VR硬件</a></p>
+          <p class="are-con-left-p2">
+          <a href="hardware/solution/">方案分析</a>
+          <a href="hardware/vive/">HTC Vive</a>
+          <a href="hardware/Oculus/">Oculus</a>
+        </p>
+        </li>
+        <li>
+          <p class="are-con-left-p1"><a>VR开发</a></p>
+          <p class="are-con-left-p2">
+          <a>Unity</a>
+          <a>UE4</a>
+          <a>CryengineV</a>
+        </p>
+        </li>
+        <li>
+          <p class="are-con-left-p1"><a>AR|MR</a></p>
+          <p class="are-con-left-p2">
+          <a>ARKit</a>
+          <a>ARCore</a>
+          <a>Hololens</a>							
+        </p>
+        </li>
+        <li>
+          <p class="are-con-left-p1"><a>VR仿真</a></p>
+          <p class="are-con-left-p2 are-con-left-p2-bornone">
+          <a>OpenFlight</a>
+          <a>OSG</a>
+          <a>互动设计软件</a>
+        </p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      arr: []
+    }
+  }
+}
+</script>
+<style scoped>
+.menu{
+  width: 250px;
+  height: 330px;
+  padding: 15px 0;
+  background: rgba(0,0,0,0.78)!important;
+}
+.menu ul li {
+  cursor: pointer;
+  padding: 17px 20px 0;
+}
+.menu ul li a{
+  color: rgb(255, 255, 255);
+}
+.are-con-left-p1 {
+  font-size: 14px;
+  margin-bottom: 5px;
+}
+.are-con-left-p2 {
+  font-size: 12px;
+  border-bottom: 1px solid rgba(255,255,255,0.3);
+  padding-bottom: 20px;
+}
+li:last-child .are-con-left-p2{
+  border-bottom: none;
+}
+.are-con-left-p2 a {
+  margin-right: 15px;
+}
+</style>
