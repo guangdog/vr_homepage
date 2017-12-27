f97089a1bdc42a04fa135c1071269f6cf9dccde1
wangchen <1634718783@qq.com>
2017-12-27 21:03:12 +0800

    主页面

--- src/views/homepage/homepage.vue
+++ src/views/homepage/homepage.vue
@@ -2,16 +2,25 @@
   <div class="content">
     <hometop></hometop>
 	  <slide1></slide1>
+	  <slide2></slide2>
+	  <!-- <slide3></slide3> -->
+	  <slide4></slide4>
   </div>
 </template>
 <script>
 import hometop from './topcontent/topcontent.vue'
 import slide1 from './slide1.vue'
+import slide2 from './slide2.vue'
+// import slide3 from './slide3.vue'
+import slide4 from './slide4.vue'
 
 export default {
   components: {
     hometop,
-    slide1
+    slide1,
+    slide2,
+    // slide3,
+    slide4
   }
 }
 </script>
