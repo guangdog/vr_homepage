cb000d5f2f983331f11897afec26681e54ab6bd2
wangchen <1634718783@qq.com>
2017-12-28 21:59:10 +0800

    homepage_api

--- src/api/homepage.js
+++ src/api/homepage.js
@@ -1,13 +1,23 @@
 import service from '../utils/service'
+// 轮播图列表
 export function bannerList () {
   return service({
     url: 'banner/list',
     methods: 'get'
   })
 }
+// 广告列表
 export function homeadsList () {
   return service({
     url: 'homeads/list',
     methods: 'get'
+  })
+}
+// 通过分类ID获取所有资讯
+export function infosGetByCateId (params) {
+  return service({
+    url: 'infos/getByCateId',
+    methods: 'get',
+    params
   })
 }
