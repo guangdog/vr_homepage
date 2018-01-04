f1bdd0ae75312b7d3f82fa25e01bafb9eef91af9
wangchen <1634718783@qq.com>
2018-01-04 20:10:51 +0800

    slide2

--- src/views/homepage/slide2.vue
+++ src/views/homepage/slide2.vue
@@ -1,24 +1,24 @@
 <template>
     <div class="slide2">
-        <item :arr="arr" :color="'#75d09f'">
+        <item :arr="dataObj.arr1" :color="'#75d09f'">
             <h4 class="rank_boxh41" slot="top">
                 <i><img src="../../assets/images/homepage/rank1.png" /></i>
                 <a href="http://www.52vr.com/forum/unreal">虚幻UE4</a>
             </h4>
         </item>
-        <item :arr="arr" :color="'#ffb85c'">
+        <item :arr="dataObj.arr2" :color="'#ffb85c'">
              <h4 class="rank_boxh42" slot="top">
                 <i><img src="../../assets/images/homepage/rank2.png" /></i>
                 <a href="http://www.52vr.com/forum/hololens">AR-Hololens</a>
             </h4>
         </item>
-        <item :arr="arr" :color="'#ff9066'">
+        <item :arr="dataObj.arr3" :color="'#ff9066'">
             <h4 class="rank_boxh43" slot="top">
                 <i><img src="../../assets/images/homepage/rank3.png" /></i>
                 <a href="http://www.52vr.com/forum/unity">Unity</a>
             </h4>
         </item>
-        <item :arr="arr" :color="'#d6ade7'">
+        <item :arr="dataObj.arr4" :color="'#d6ade7'">
             <h4 class="rank_boxh44" slot="top">
                 <i><img src="../../assets/images/homepage/rank4.png" /></i>
                 <a href="http://www.52vr.com/forum/models">模型库</a>
@@ -28,6 +28,7 @@
 </template>
 <script>
 import item from './slide2/item.vue'
+import {bbsinfosListByPage} from '../../api/homepage'
 export default{
   components: {
     item
@@ -35,18 +36,42 @@ export default{
   data () {
     return {
       arr: [
-        {tname: '虚幻氪金VR逆袭', timg: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2232321279,3451331718&fm=27&gp=0.jpg', aaa: '321'},
-        {tname: '虚幻氪金VR逆袭', timg: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2232321279,3451331718&fm=27&gp=0.jpg', aaa: '12'},
-        {tname: '虚幻氪金VR逆袭', timg: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2232321279,3451331718&fm=27&gp=0.jpg', aaa: '11'},
-        {tname: '虚幻氪金VR逆袭', timg: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2232321279,3451331718&fm=27&gp=0.jpg', aaa: '32'},
-        {tname: '虚幻氪金VR逆袭', timg: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2232321279,3451331718&fm=27&gp=0.jpg', aaa: '22'},
-        {tname: '虚幻氪金VR逆袭', timg: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2232321279,3451331718&fm=27&gp=0.jpg', aaa: '44'},
-        {tname: '虚幻氪金VR逆袭', timg: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2232321279,3451331718&fm=27&gp=0.jpg', aaa: '66'},
-        {tname: '虚幻氪金VR逆袭', timg: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2232321279,3451331718&fm=27&gp=0.jpg', aaa: '66'},
-        {tname: '虚幻氪金VR逆袭', timg: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2232321279,3451331718&fm=27&gp=0.jpg', aaa: '66'},
-        {tname: '虚幻氪金VR逆袭', timg: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2232321279,3451331718&fm=27&gp=0.jpg', aaa: '66'}
-      ]
+        {title: '虚幻氪金VR逆袭', thumbnail: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2232321279,3451331718&fm=27&gp=0.jpg', pv: '321'},
+        {title: '虚幻氪金VR逆袭', thumbnail: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2232321279,3451331718&fm=27&gp=0.jpg', pv: '12'},
+        {title: '虚幻氪金VR逆袭', thumbnail: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2232321279,3451331718&fm=27&gp=0.jpg', pv: '11'},
+        {title: '虚幻氪金VR逆袭', thumbnail: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2232321279,3451331718&fm=27&gp=0.jpg', pv: '32'},
+        {title: '虚幻氪金VR逆袭', thumbnail: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2232321279,3451331718&fm=27&gp=0.jpg', pv: '22'},
+        {title: '虚幻氪金VR逆袭', thumbnail: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2232321279,3451331718&fm=27&gp=0.jpg', pv: '44'},
+        {title: '虚幻氪金VR逆袭', thumbnail: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2232321279,3451331718&fm=27&gp=0.jpg', pv: '66'},
+        {title: '虚幻氪金VR逆袭', thumbnail: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2232321279,3451331718&fm=27&gp=0.jpg', pv: '66'},
+        {title: '虚幻氪金VR逆袭', thumbnail: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2232321279,3451331718&fm=27&gp=0.jpg', pv: '66'},
+        {title: '虚幻氪金VR逆袭', thumbnail: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2232321279,3451331718&fm=27&gp=0.jpg', pv: '66'}
+      ],
+      dataObj: {
+        arr1: [],
+        arr2: [],
+        arr3: [],
+        arr4: []
+      }
     }
+  },
+  methods: {
+    query (i, id) {
+      var listQuery = {
+        page: 1,
+        limit: 10,
+        category_id: id
+      }
+      bbsinfosListByPage(listQuery).then(res => {
+        this.dataObj['arr' + i] = res.data.data
+      })
+    }
+  },
+  created () {
+    this.query(1, 16)
+    this.query(2, 33)
+    this.query(3, 15)
+    this.query(4, 11)
   }
 }
 </script>
