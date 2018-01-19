02ca6cfdf87dda1609eab65d192c3397847bb4cd
wangchen <1634718783@qq.com>
2018-01-19 15:20:54 +0800

    serviece

--- src/utils/service.js
+++ src/utils/service.js
@@ -1,6 +1,7 @@
-import axios from 'axios'
+﻿import axios from 'axios'
 const service = axios.create({
   baseURL: 'http://114.55.249.153:7002',
   timeout: 5000 // 请求超时时间
+  // withCredentials: true
 })
 export default service
